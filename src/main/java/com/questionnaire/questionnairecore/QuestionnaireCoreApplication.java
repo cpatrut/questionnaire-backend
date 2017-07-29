3ac7f22c1ce96b1c2eaaafcecb9dec2d6a08b9c8
Catalin Patrut <cpatrut@outlook.com>
2017-07-29 19:03:35 +0300

    initial commit, nothing interesting

---
+++ src/main/java/com/questionnaire/questionnairecore/QuestionnaireCoreApplication.java
@@ -0,0 +1,12 @@
+package com.questionnaire.questionnairecore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionnaireCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionnaireCoreApplication.class, args);
+	}
+}
